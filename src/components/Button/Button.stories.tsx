7d019681b149ebb4e6aa298b70318d37abb91f5a
justin.lin <justin.lin@yxunistar.com>
2021-05-07 16:16:47 +0800

    feat: modify story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react/types-6-0';
-import Button , { ButtonProps } from './Button';
+import React from "react";
+import {Story, Meta} from "@storybook/react/types-6-0";
+import Button, {ButtonProps} from "./Button";
 import "../../styles/index.scss";
 
 export default {
-  title: 'Button',
-  component: Button,
+    title: "Button",
+    component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = args => {
+    return <Button {...args} />;
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
-  btnType: 'danger',
-  children: "确定"
+    btnType: "danger",
+    children: "确定",
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,65 +1,50 @@
-import React from 'react';
+import React from "react";
 import classNames from "classnames";
 
-export type ButtonSize = 'lg' | 'sm'
-export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+export type ButtonSize = "lg" | "sm";
+export type ButtonType = "primary" | "default" | "danger" | "link";
 
 interface BaseButtonProps {
-  className: string;
-  disabled: boolean;
-  size: ButtonSize;
-  btnType: ButtonType;
-  children: React.ReactNode;
-  href: string;
+    className: string;
+    disabled: boolean;
+    size: ButtonSize;
+    btnType: ButtonType;
+    children: React.ReactNode;
+    href: string;
+    onClickHandler: () => void;
 }
 
 type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
 type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>;
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
-const Button: React.FC<ButtonProps> = (props)=>{
-  const {
-    btnType,
-    disabled,
-    size,
-    children,
-    className,
-    href,
-    ...restProps
-  } = props;
+const Button: React.FC<ButtonProps> = props => {
+    const {btnType, disabled, size, children, className, href, onClickHandler, ...restProps} = props;
 
-  const classes = classNames('btn', className, {
-    [`btn-${btnType}`]: btnType,
-    [`btn-${size}`]: size,
-    'disabled': (btnType === 'link') && disabled
-  })
+    const classes = classNames("btn", className, {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        disabled: btnType === "link" && disabled,
+    });
 
-  if(btnType === 'link' && href){
-    return (
-      <a
-        {...restProps}
-        href={href}
-        className={classes}
-      >
-        {children}
-      </a>
-    )
-  } else {
-    return (
-      <button
-        {...restProps}
-        className={classes}
-        disabled={disabled}
-      >
-        {children}
-      </button>
-    )
-  }
-}
+    if (btnType === "link" && href) {
+        return (
+            <a {...restProps} href={href} className={classes}>
+                {children}
+            </a>
+        );
+    } else {
+        return (
+            <button {...restProps} className={classes} disabled={disabled} onClick={onClickHandler}>
+                Button
+            </button>
+        );
+    }
+};
 
 Button.defaultProps = {
-  disabled: false,
-  btnType: 'default'
-}
+    disabled: false,
+    btnType: "default",
+};
 
 export default Button;
